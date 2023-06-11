616e52738a85480c6e8ec68e15e688c3865f0ebb
jenisys <jenisys@users.noreply.github.com>
2023-06-11 11:22:52 +0200

    check_cmake-presets: Update comments/docs.

--- bin/check_cmake-presets.sh
+++ bin/check_cmake-presets.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# SEE:    https://cmake.org/cmake/help/latest/manual/cmake-presets.7.html#schema
+# =============================================================================
+# check_cmake-presets.sh
+# =============================================================================
 # REQUIRES: pip install check-jsonschema
+# USAGE:
+#   check_cmake-presets.sh                      # CHECK: CMakePresets.json (implicitl)
+#   check_cmake-presets.sh CMakePresets.json    # CHECK: CMakePresets.json (explicit)
+#   check_cmake-presets.sh CMakePresets1.json ...  # CHECK: Many cmake-preset files
+#
+# SEE ALSO:
+#  * https://cmake.org/cmake/help/latest/manual/cmake-presets.7.html#schema
+#  * https://cmake.org/cmake/help/latest/manual/cmake-presets.7.html#id13
+#
+# SCHEMA-FILE:
+#  * https://gitlab.kitware.com/cmake/cmake/-/raw/master/Help/manual/presets/schema.json
+# =============================================================================
 
 set -e
 HERE=$(dirname $0)
@@ -15,3 +29,5 @@ for CMAKE_PRESETS_FILE in ${CMAKE_PRESETS_FILES}; do
     printf "${CMAKE_PRESETS_FILE}  ... "
     check-jsonschema --schemafile ${HERE}/cmake-presets.schema.json --verbose ${CMAKE_PRESETS_FILE}
 done
+
+
